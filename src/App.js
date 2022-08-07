29aebae767bfb7af21706799eda32650e60f4f62
HoiLen <len1231.hoi@gmail.com>
2022-08-07 22:32:02 +0900

    ReactRouter was installed.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import {Article} from './components/index'
+import { Routes, Route, Link } from "react-router-dom"
+import { Home, Login, Register, NotFound } from './components/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>HoiLen</h1>
 
-      <Article />
-    </div>
+      <nav>
+			  <ul>
+				  <li><Link to="/">Home</Link></li>
+				  <li><Link to="/register">Register</Link></li>
+				  <li><Link to="/login/">--Login--</Link></li>
+			  </ul>
+		  </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register/" element={<Register />} />
+        <Route path="/login/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 

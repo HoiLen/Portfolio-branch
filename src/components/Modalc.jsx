87b9b1bab185c09b7b04b1a5c869f9e2841c4629
HoiLen <len1231.hoi@gmail.com>
2022-08-11 18:15:19 +0900

    余分なimport文を削除しました。

--- src/components/Modalc.jsx
+++ src/components/Modalc.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
-import { Link, useParams } from 'react-router-dom'
-import { IMAGES } from '../images'
+import { useParams } from 'react-router-dom'
 
 const Modalc = () => {
 
